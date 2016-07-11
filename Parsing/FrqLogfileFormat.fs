5d51f94e133539dfaae81e539317eac5e1b3e5f3
Raphael Mader <RaphMad@users.noreply.github.com>
2016-07-11 22:28:56 +0200

    Implemented basic parsing

--- Parsing/FrqLogfileFormat.fs
+++ Parsing/FrqLogfileFormat.fs
@@ -5,33 +5,52 @@ open FParsec
 module FrqLogfileFormat =
     type Severity = DEBUG | INFO | WARN | ERROR | FATAL
 
-    type LogEntry = {
-        Timestamp: System.DateTime;
-        Severity: Severity;
-        ProcessId: int;
-        Title: string;
-        Message: string;
-    }
+    type LogEntry = { 
+        Timestamp: System.DateTime
+        Severity: Severity
+        ProcessId: int
+        Title: string
+        Message: string
+        }
 
-    let space : Parser<unit,unit> = skipChar ' '
-    let period : Parser<unit,unit> = skipChar '.'
-    let colon : Parser<unit,unit> = skipChar ':'
-    let comma : Parser<unit,unit> = skipChar ','
 
-    let pipe7 p1 p2 p3 p4 p5 p6 p7 fn =
-        p1 >>= fun a ->
-        p2 >>= fun b ->
-        p3 >>= fun c ->
-        p4 >>= fun d ->
-        p5 >>= fun e ->
-        p6 >>= fun f ->
-        p7 >>= fun g -> preturn (fn a b c d e f g)
+    let private space : Parser<unit,unit> = skipChar ' '
+    let private period = skipChar '.'
+    let private colon = skipChar ':'
+    let private comma = skipChar ','
 
-    let timestamp = pipe7 pint32 .>> period // day
-                          pint32 .>> period // month
-                          pint32 .>> space // year
-                          pint32 .>> colon // hour
-                          pint32 .>> colon // minute
-                          pint32 .>> comma // second
-                          pint32
-                          (fun day month year hour minute second -> new System.DateTime(day, month, year, hour, minute, second))
+    let private timestamp = pint32 .>> period >>= fun day ->
+                    pint32 .>> period >>= fun month ->
+                    pint32 .>> space  >>= fun year ->
+                    pint32 .>> colon  >>= fun hour ->
+                    pint32 .>> colon  >>= fun minute ->
+                    pint32 .>> comma  >>= fun second ->
+                    pint32            |>> fun millisecond -> 
+                    new System.DateTime(year, month, day, hour, minute, second, millisecond)
+
+    let private severity : Parser<Severity, unit>  = choice [stringReturn "DEBUG" DEBUG
+                                                             stringReturn "INFO " INFO
+                                                             stringReturn "WARN " WARN
+                                                             stringReturn "ERROR" ERROR
+                                                             stringReturn "FATAL" FATAL]
+
+    let private processId : Parser<int, unit> = skipChar 'P' >>. pint32
+
+    let private title = pstring
+
+    let private message = pstring
+
+    let private sep = skipString "; "
+
+    let private logEntry = tuple5 (timestamp .>> sep)
+                                  (severity .>> sep)
+                                  (processId .>> sep)
+                                  (charsTillString "; " true System.Int32.MaxValue)
+                                  (restOfLine true)
+                                  |>> fun (t, s, p, ti, m) -> { Timestamp = t; Severity = s; ProcessId = p; Title = ti; Message = m}
+
+    let private parseLine line = match run logEntry line with
+                                     | Success (result, _, _) -> Choice1Of2 result
+                                     | Failure (error, _, _)  -> Choice2Of2 error
+
+    let parse filePath = System.IO.File.ReadLines(filePath) |> Seq.map parseLine
---
+++ Presentation/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Presentation
+{
+   using System.Linq;
+   using System.Windows;
+   using Microsoft.FSharp.Core;
+   using Parsing;
+
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App
+   {
+      /// <summary>
+      /// Handles the startup.
+      /// </summary>
+      /// <param name="sender">The sender.</param>
+      /// <param name="e">The <see cref="System.Windows.StartupEventArgs"/> instance containing the event data.</param>
+      private void HandleStartup(object sender, StartupEventArgs e)
+      {
+         var test = FrqLogfileFormat.parse("input.txt");
+
+         var success = test.Where(x => x.IsChoice1Of2).ToArray();
+         var error = test.Where(x => x.IsChoice2Of2).ToArray();
+      }
+   }
+}
