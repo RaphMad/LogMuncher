f920bf0db758e8df4db271f46797b5b236398d79
Raphael Mader <RaphMad@users.noreply.github.com>
2016-07-11 16:10:05 +0200

    Added basic parsing

--- Parsing/FrqLogFormat.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace Parsing
-
-type Class1() = 
-    member this.X = "F#"
---
+++ Parsing/FrqLogfileFormat.fs
@@ -0,0 +1,37 @@
+﻿namespace Parsing
+
+open FParsec
+
+module FrqLogfileFormat =
+    type Severity = DEBUG | INFO | WARN | ERROR | FATAL
+
+    type LogEntry = {
+        Timestamp: System.DateTime;
+        Severity: Severity;
+        ProcessId: int;
+        Title: string;
+        Message: string;
+    }
+
+    let space : Parser<unit,unit> = skipChar ' '
+    let period : Parser<unit,unit> = skipChar '.'
+    let colon : Parser<unit,unit> = skipChar ':'
+    let comma : Parser<unit,unit> = skipChar ','
+
+    let pipe7 p1 p2 p3 p4 p5 p6 p7 fn =
+        p1 >>= fun a ->
+        p2 >>= fun b ->
+        p3 >>= fun c ->
+        p4 >>= fun d ->
+        p5 >>= fun e ->
+        p6 >>= fun f ->
+        p7 >>= fun g -> preturn (fn a b c d e f g)
+
+    let timestamp = pipe7 pint32 .>> period // day
+                          pint32 .>> period // month
+                          pint32 .>> space // year
+                          pint32 .>> colon // hour
+                          pint32 .>> colon // minute
+                          pint32 .>> comma // second
+                          pint32
+                          (fun day month year hour minute second -> new System.DateTime(day, month, year, hour, minute, second))
